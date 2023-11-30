56a874786c08412d18b3eb89729d56230d434b03
solomoncyj <solomoncyj@hotmail.com>
2023-11-30 23:54:54 +0800

    Qol 1

--- cmd/install.go
+++ cmd/install.go
@@ -9,21 +9,37 @@ import (
 
 	"github.com/spf13/cobra"
 
-	utils "github.com/betterdiscord/cli/utils"
+	"github.com/betterdiscord/cli/utils"
 )
 
+func strinlist(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
 func init() {
 	rootCmd.AddCommand(installCmd)
 }
+func die(message string) {
+	fmt.Println(message)
+	os.Exit(1)
+}
 
+var ValidArgs = []string{"canary", "stable", "ptb"}
 var installCmd = &cobra.Command{
 	Use:       "install <channel>",
 	Short:     "Installs BetterDiscord to your Discord",
 	Long:      "This can install BetterDiscord to multiple versions and paths of Discord at once. Options for channel are: stable, canary, ptb",
-	ValidArgs: []string{"canary", "stable", "ptb"},
+	ValidArgs: ValidArgs,
 	Args:      cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
 		var releaseChannel = args[0]
+		if !strinlist(releaseChannel, ValidArgs) {
+			die("invalid arguments given. valid arguments are: " + strings.Join(ValidArgs, ", "))
+		}
 		var targetExe = ""
 		switch releaseChannel {
 		case "stable":
@@ -36,31 +52,31 @@ var installCmd = &cobra.Command{
 			targetExe = "DiscordPTB.exe"
 			break
 		default:
-			targetExe = ""
+			targetExe = "Discord.exe"
 		}
 
 		// Kill Discord if it's running
 		var exe = utils.GetProcessExe(targetExe)
 		if len(exe) > 0 {
 			if err := utils.KillProcess(targetExe); err != nil {
-				fmt.Println("Could not kill Discord")
+				die("Could not kill Discord")
 				return
 			}
 		}
 
 		// Make BD directories
 		if err := os.MkdirAll(utils.Data, 0755); err != nil {
-			fmt.Println("Could not create BetterDiscord folder")
+			die("Could not create BetterDiscord folder")
 			return
 		}
 
 		if err := os.MkdirAll(utils.Plugins, 0755); err != nil {
-			fmt.Println("Could not create plugins folder")
+			die("Could not create plugins folder")
 			return
 		}
 
 		if err := os.MkdirAll(utils.Themes, 0755); err != nil {
-			fmt.Println("Could not create theme folder")
+			die("Could not create theme folder")
 			return
 		}
 
@@ -68,7 +84,7 @@ var installCmd = &cobra.Command{
 		var apiData, err = utils.DownloadJSON[utils.Release]("https://api.github.com/repos/BetterDiscord/BetterDiscord/releases/latest")
 		if err != nil {
 			fmt.Println("Could not get API response")
-			fmt.Println(err)
+			die(err.Error())
 			return
 		}
 
@@ -86,7 +102,7 @@ var installCmd = &cobra.Command{
 		var asarPath = path.Join(utils.Data, "betterdiscord.asar")
 		err = utils.DownloadFile(downloadUrl, asarPath)
 		if err != nil {
-			fmt.Println("Could not download asar")
+			die("Could not download asar")
 			return
 		}
 
@@ -98,14 +114,14 @@ var installCmd = &cobra.Command{
 		indString = indString + "\nmodule.exports = require(\"./core.asar\");"
 
 		if err := os.WriteFile(path.Join(corePath, "index.js"), []byte(indString), 0755); err != nil {
-			fmt.Println("Could not write index.js in discord_desktop_core!")
+			die("Could not write index.js in discord_desktop_core!")
 			return
 		}
 
 		// Launch Discord if we killed it
 		if len(exe) > 0 {
 			var cmd = exec.Command(exe)
-			cmd.Start()
+			_ = cmd.Start()
 		}
 	},
 }
